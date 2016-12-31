a6bc4e02187d180a9e33244d7b85cbbbac1e7bd9
Teameureka <rreynolds@teameureka.net>
2016-12-31 12:11:28 -0700

    Move
    moved new hardware config examples to ExampleCode folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCode/TestHardwareTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCode/TestHardwareTeleOp.java
@@ -1,10 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.ExampleCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.ExampleCode.MyBotHardwareSetup;
 
 
 /**
